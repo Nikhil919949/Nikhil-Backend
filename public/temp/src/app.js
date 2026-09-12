1a63ece4d0f22385ed9582c5270b4a5183a06ac5
Nikhil919949 <nikhilgupta4287@gmail.com>
2026-09-12 18:41:27 +0530

    Postman with cloudnary used and mongoDb connection

--- public/temp/src/app.js
+++ public/temp/src/app.js
@@ -3,8 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app=express()
 app.use(cors({
-    origin : process.env.CORS_ORIGIN
-    credentials:true
+    origin : process.env.CORS_ORIGIN 
+     credentials : true
 }))
 app.use(express.json({limit :"16kb"}))
 app.use(express.urlencoded({extended:true,limit :"16kb"}))
@@ -17,6 +17,6 @@ import userRouter from './routes/user.routes.js'
 //route declaration
 app.use("/api/v1/users",useRouter)
 
-http://localhost:8000/api/v1/users/register
+//http://localhost:8000/api/v1/users/register
 
  export {app}
